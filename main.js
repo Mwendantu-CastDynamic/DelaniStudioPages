4ec363dde08975b7afa0b79339bd9174e0f4a457
Brian Muchunku <brauxer@gmail.com>
2021-09-14 04:08:52 +0300

    Formulate and debug JQuery toggle functionality code for the 'what we do' section code

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+$(document).ready(function(){
+
+  function pulsate(element1, elementDescription2 ){
+
+    $(element1 || this).animate({opacity:0},300, function(){
+        $(element1).hide();
+        $(elementDescription2).show().animate({opacity:1},300);
+        
+  
+    });
+  
+  }
+  
+  $('.what-we-do-icon').click(function(evt){
+      const clickedElement = $(evt.target).hasClass('what-we-do-icon')? $(clickedElement) : $(clickedElement).parent();
+      pulsate(clickedElement, $(clickedElement).prev());
+
+  });
+
+});
